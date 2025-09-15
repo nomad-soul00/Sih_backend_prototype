fc67e004291b9683edc385f123c68720eeb78bf4
Nomad-soul00 <sahilahamed813@gmail.com>
2025-09-15 21:16:01 +0530

    SPA routing

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.get('/', (req,res)=>{
     res.send("Hello");
 })
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 });
