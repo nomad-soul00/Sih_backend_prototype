892b0de2511ca1da91b696f2ac9ad2462f1e3fb8
Nomad-soul00 <sahilahamed813@gmail.com>
2025-09-16 19:32:07 +0530

    f

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import path from 'path';
-import dotenv from 'dotenv'
-import cors from 'cors'
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 import dataRouter from './routes/Data.routes.js';
 import computeRouter from './routes/Compute.routes.js';
@@ -21,24 +27,23 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.resolve('public')));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 
 app.use('/api/v1/data', dataRouter);
 app.use('/api/v1/compute', computeRouter);
 
-
-
-app.get('/', (req,res)=>{
-    res.send("Hello");
-})
+app.get('/', (req, res) => {
+  res.send("Hello");
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-// module.exports = app
