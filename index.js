13df699ca20743112cb04d402ab0380953ba1673
Nomad-soul00 <sahilahamed813@gmail.com>
2025-09-16 19:36:28 +0530

    cors

--- index.js
+++ index.js
@@ -20,11 +20,24 @@ const app = express();
 
 const allowedOrigins = ['https://sih-fronted-prototype.vercel.app'];
 
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST'],
-  credentials: true
-}));
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      return callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
